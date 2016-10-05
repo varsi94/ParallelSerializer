5dcec8286b3a782e50e65e9387102216b22555f8
varsi94 <varsi.marci@gmail.com>
2016-10-05 17:58:06 +0200

    Add project files.

---
+++ DemoModel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoModel
+{
+    public class Class1
+    {
+    }
+}
