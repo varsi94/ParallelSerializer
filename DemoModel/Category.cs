7f89905b81157d770ca5c0a0d874b9c69944ed11
varsi94 <varsi.marci@gmail.com>
2016-10-05 18:14:38 +0200

    added demo classes

---
+++ DemoModel/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DemoModel
+{
+    public class Category
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ DemoModel/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoModel
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public int ID { get; set; }
+
+        public int Count { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
