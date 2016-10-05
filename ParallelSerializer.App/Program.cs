9334d33c5348b88b898deeb7727deac59c1cfcf4
varsi94 <varsi.marci@gmail.com>
2016-10-05 18:55:02 +0200

    Added console and parallelserializer projects

---
+++ ParallelSerializer.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using DemoModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelSerializer.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParallelSerializer serializer = new ParallelSerializer();
+            using (var ms = new MemoryStream())
+            {
+                var product = new Product
+                {
+                    Name = "asd",
+                    ID = 1,
+                    Category = new Category {Name = "asdasd"},
+                    Count = 5
+                };
+                serializer.Serialize(product, ms);
+            }
+        }
+    }
+}
---
+++ ParallelSerializer/ParallelSerializer.cs
@@ -0,0 +1,49 @@
+﻿using DemoModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelSerializer
+{
+    public class ParallelSerializer
+    {
+        public void Serialize(object obj, Stream output)
+        {
+        }
+
+        public Product Deserialize(Stream input)
+        {
+            using (var br = new BinaryReader(input))
+            {
+                int id = br.ReadInt32();
+                if (id != 15)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return null;
+            }
+        }
+
+        private void Serialize(Product product, Stream output)
+        {
+            using (var bw = new BinaryWriter(output))
+            {
+                if (product == null)
+                {
+                    bw.Write(-1);
+                    return;
+                }
+                bw.Write(15);
+                bw.Write((product.Category == null) ? -1 : 16);
+                if (product.Category != null)
+                {
+                    
+                }
+            }
+        }
+    }
+}
